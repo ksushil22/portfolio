e1beea6c4079c8989d860ffc0c527a4ed1d7452c
insightSush <kainths@algonquincollege.com>
2024-04-09 01:02:37 -0400

    issue-1: complete project'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,63 @@
+import {HeroParallax} from "@/pages/ui/hero-parallax";
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import {HoverEffect} from "@/pages/ui/card-hover-effect";
+import {TracingBeam} from "@/pages/ui/tracing-beam";
+import {cn} from "@/pages/utils/cn";
+import React from "react";
+import {CardsSplitDelay} from "@/pages/ui/card-split";
+import {Tabs} from "@/pages/ui/tabs";
+import {EDUCATION_CARDS, Highlight, PRODUCTS, PROFESSIONAL_SUMMARY, PROJECTS} from "@/pages/utils/constants";
 import Image from "next/image";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <PerfectScrollbar options={{minScrollbarLength: 100}}>
+            <TracingBeam className={"z-50"}>
+                <div
+                    style={{
+                        position: "absolute",
+                        top: 20,
+                        right: 20,
+                        zIndex: 2,
+                        display: 'flex',
+                        alignItems: 'center'
+                    }}>
+                    <a href={"/Resume.pdf"} download={"Resume.pdf"}><Highlight>Download Resume</Highlight></a> |
+                    <a href={"mailto:kainths.2206@gmail.com"}><Highlight>Get in touch!</Highlight></a>
+                    <a href={"https://www.linkedin.com/in/sushil-kainth/"} target={"_blank"}><img src={"/linkedin.svg"} width={50} height={50} alt={"linkedin"} /></a>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                <div
+                    className={"no-visible-scrollbar antialiased"}
+                    style={{
+                        backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8)), url(/background.png)',
+                        backgroundSize: '50% 25%',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'right'
+                    }}>
+                    <HeroParallax products={PRODUCTS}/>
+                </div>
+                <div className="h-[100vh] items-center w-full education">
+                    <h1 className={"text-center text-7xl top-0"}>Education</h1>
+                    <div style={{
+                        marginTop: 250
+                    }}>
+                        <CardsSplitDelay cards={EDUCATION_CARDS}/>
+                    </div>
+                </div>
+                <div className={'min-h-[120vh]'}>
+                    <h1 className={"text-center text-7xl top-0"}>Professional Summary</h1>
+                    <Tabs tabs={PROFESSIONAL_SUMMARY}/>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                </div>
+                <h1 className={"text-center text-7xl top-0"}>Projects</h1>
+                <div className="max-w-5xl mx-auto px-8 m-[30px]">
+                    <HoverEffect items={PROJECTS}/>
+                </div>
+            </TracingBeam>
+        </PerfectScrollbar>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    )
 }
---
+++ src/pages/utils/constants.tsx
@@ -0,0 +1,306 @@
+import React from "react";
+import {cn} from "@/pages/utils/cn";
+
+export const Highlight = ({
+                              children,
+                              className,
+                          }: {
+    children: React.ReactNode;
+    className?: string;
+}) => {
+    return (
+        <span
+            className={cn(
+                "font-bold bg-emerald-100 text-emerald-700 dark:bg-emerald-700/[0.2] dark:text-emerald-500 px-1 py-0.5",
+                className
+            )}
+        >
+      {children}
+    </span>
+    );
+};
+
+const Experience = ({
+    heading,
+    duration,
+    accomplishements
+}:{
+    heading: string,
+    duration: string,
+    accomplishements: React.JSX.Element[]
+}) => {
+    return <div className={"rounded-2xl p-10 text-white bg-teal-800 left-[20%] min-h-[70vh]"}>
+        <Heading>{heading}</Heading>
+        <Duration>{duration}</Duration>
+        <hr />
+        <h1 className={"text-xl text-center"} style={{
+            marginTop: 30
+        }}>Accomplishments:</h1>
+        <Accomplishments accomplishments={accomplishements} />
+    </div>
+};
+export const Heading = ({children}: {children: React.ReactNode}) => {
+    return <h1 className={"text-2xl text-center font-bold"}>{children}</h1>
+};
+
+export const Duration = ({children}: {children: React.ReactNode}) => {
+    return <h1 className={"text-xl text-center"}>{children}</h1>
+};
+
+export const Accomplishments = ({accomplishments}: {accomplishments: React.JSX.Element[]}) => {
+    return <ol style={{
+        position: 'relative',
+        top: 20
+    }}>
+        {accomplishments.map(accomplishment => {
+            return <li style={{
+                position: 'relative',
+                marginLeft: '20%',
+                marginRight: '20%',
+                marginTop: 10
+            }}>{accomplishment}</li>
+        })}
+    </ol>
+
+};
+export const PRODUCTS = [
+    {
+        title: "C#",
+        link: "https://gomoonbeam.com",
+        thumbnail:
+            "/cs.png",
+    },
+    {
+        title: "Python",
+        link: "https://google.com",
+        thumbnail:
+            "/python.png",
+    },
+    {
+        title: "Java",
+        link: "https://userogue.com",
+        thumbnail:
+            "/java.png",
+    },
+    {
+        title: "JavaScript",
+        link: "https://editorially.org",
+        thumbnail:
+            "/js.png",
+    },
+    {
+        title: "JavaScript",
+        link: "https://editrix.ai",
+        thumbnail:
+            "/js.png",
+    },
+    {
+        title: "HTML",
+        link: "https://app.pixelperfect.quest",
+        thumbnail:
+            "/html.png",
+    },
+
+    {
+        title: "CSS",
+        link: "https://algochurn.com",
+        thumbnail:
+            "/css.png",
+    },
+    {
+        title: "React",
+        link: "https://ui.aceternity.com",
+        thumbnail:
+            "/react.png",
+    },
+    {
+        title: "Angular",
+        link: "https://tailwindmasterkit.com",
+        thumbnail:
+            "/angular.svg",
+    },
+    {
+        title: "Nodejs",
+        link: "https://smartbridgetech.com",
+        thumbnail:
+            "/nodejs.png",
+    },
+    {
+        title: "Spring",
+        link: "https://renderwork.studio",
+        thumbnail:
+            "/spring.png",
+    },
+
+    {
+        title: "Machine Learning",
+        link: "https://cremedigital.com",
+        thumbnail:
+            "/ml.jpg",
+    },
+    {
+        title: "Spring",
+        link: "https://goldenbellsacademy.com",
+        thumbnail:
+            "/spring.png",
+    },
+    {
+        title: "Django",
+        link: "https://invoker.lol",
+        thumbnail:
+            "/django.png",
+    },
+    {
+        title: "Artificial Intelligence",
+        link: "https://efreeinvoice.com",
+        thumbnail:
+            "/ai.png",
+    },
+];
+
+export const PROJECTS = [
+    {
+        title: "Dough Me A flavour",
+        description: "E-Commerce website for a bakery shop. Used ReactJs, Spring, Bootstrap, SQL. Functionality includes" +
+            ": Ordering, Cart, Live product count, and Reviews"
+    },
+    {
+        title: "Размещения (Placement)",
+        description: "A placement platform for the university to post about the upcoming placement drives and students to " +
+            "discuss about the opportunity with each other and with professors."
+    },
+    {
+        title: "Smart Switch",
+        description: "A smart switch made using IOT with webapp to switch button over LAN. Could be accessed by" +
+            " anyone on the network. Shows the current status of the switches at home."
+    },
+    {
+        title: "Smart Face Detection System",
+        description: "Smart face detection system that uses Reinforcement Learning and Computer vision" +
+            "to automatically adjust and save the camera's properties according to the environment"
+    },
+    {
+        title: 'News Bot',
+        description: "Trained bot to reply to questions related to news with 5M+ dataset. Acted just like ChatGPT but, with " +
+            "less data."
+    },
+    {
+        title: "Smart Bot",
+        description: "Used ROS(Robot operation System), and iCreate3 robot. Trained the robot to roam freely in a closed " +
+            "area drawn using a marker."
+    },
+    {
+        title: "Portfolio",
+        description: "The Website that you are enjoying. Used NextJS. Try to interact with every component."
+    },
+    {
+        title: "Chess",
+        description: "Created an online multiplayer chess game for a client. Has features like: Leaderboard, " +
+            "Chess engine for detecting correct moves, chat, friend requests, and player invitations via email."
+    }
+];
+
+export const EDUCATION_CARDS = [
+    {
+        id: 1,
+        title: "High School",
+        duration: "2015 - 2017",
+        description: (
+            <p>
+                Majored in <Highlight>Science</Highlight> and <Highlight>Maths</Highlight>.
+            </p>
+        ),
+        background: '/mgn.jpg',
+        institute: "MGN Public School"
+    },
+    {
+        id: 2,
+        title: "Bachelors",
+        duration: "2017 - 2021",
+        description: (
+            <p>
+                Majored in <Highlight>Computer Science and Engineering</Highlight>.
+            </p>
+        ),
+        background: '/chitkara.jpeg',
+        institute: 'Chitkara University'
+    },
+    {
+        id: 3,
+        title: "Graduate Certificate",
+        duration: "2022 - 2023",
+        description: (
+            <p>
+                Majored in <Highlight>Artificial Intelligence</Highlight>.
+            </p>
+        ),
+        background: '/algonquin.jpg',
+        institute: 'Algonquin College'
+    },
+];
+
+export const PROFESSIONAL_SUMMARY = [
+    {
+        title: "SLiDE Lab",
+        value: 0,
+        content: <Experience
+            heading={"Full Stack Developer"}
+            duration={"January, 2023 - Current"}
+            accomplishements={[
+                (<p>- Represented company's projects in multiple <Highlight>Conferences</Highlight> including <Highlight>ReAction, Higher Education Summit </Highlight>, and <Highlight>CiCan</Highlight></p>),
+                (<p>- Formulated <Highlight>Complex SQL Queries</Highlight> for improving the processing time from <Highlight>15-20mins to a few seconds</Highlight> by reducing DB calls and </p>),
+                (<p>- Helped identify, and solve problems with the <Highlight>ML algorithms</Highlight> producing results for <Highlight>Systematic Review platform (InsightScope)</Highlight> increasing the </p>),
+                (<p>- Created <Highlight>Patient Engagement Platform</Highlight> for <Highlight>CHEO (Children's Hospital of Eastern Ontario)</Highlight></p>),
+                (<p>- Solely responsible for creating a big feature for <Highlight>CCCTG (Canadian Care Trials Group)</Highlight> from Information Gathering to Deployment of the application.</p>),
+                (<p>- Worked closely with <Highlight>Researchers, and Doctors</Highlight> to constantly improve <Highlight>Systematic Review Platforms</Highlight> by performing multiple stress testings on the algorithms <Highlight>resulting in better outputs.</Highlight></p>),
+            ]} />
+
+    },
+    {
+        title: "Aspire Software",
+        value: 1,
+        content: <Experience
+            heading={"Software Developer"}
+            duration={"July, 2021 - September, 2022"}
+            accomplishements={[
+                (<p>- Collaborated with <Highlight>product managers, UI/UX designers, and other developers</Highlight> to design, develop, and implement features based on technical specifications.</p>),
+                (<p>- Troubleshooted and resolved bugs within Spring Boot applications, ensuring smooth system operation.</p>),
+                (<p>- Led the successful <Highlight>migration of desktop applications to a cloud-based architecture</Highlight> using AWS, resulting in 62% reduction in infrastructure costs and increased scalability.</p>),
+                (<p>- Implemented <Highlight>Java-based Keycloak authentication</Highlight>, achieving a 70% performance improvement.</p>),
+                (<p>- Developed a <Highlight>user interaction tracking</Highlight> feature to <Highlight>analyze user behavior and inform product development decisions.</Highlight></p>),
+                (<p>- Led a team of 5 developers in delivering project on time and within budget.</p>),
+                (<p>- <Highlight>Optimized resource allocation and system performance</Highlight> by leveraging AWS Cloud technologies, leading to cost reduction and a 50% improvement in system performance.</p>)
+            ]} />
+
+    },
+    {
+        title: "MFNN Games",
+        value: 2,
+        content: <Experience
+            heading={"Project Manager"}
+            duration={"January, 2023 - May, 2023"}
+            accomplishements={[
+                (<p>- Established and implemented <Highlight>quality assurance</Highlight> processes to ensure <Highlight>project deliverables meet quality standards and customer expectations.</Highlight></p>),
+                (<p>- Creating and managing <Highlight>project budgets, allocating resources efficiently, and monitoring expenditures</Highlight> to ensure projects are delivered within financial constraints.</p>),
+                (<p>- Identified, assessed, and mitigated <Highlight>project risks to minimize disruptions and ensure successful project delivery.</Highlight></p>),
+                (<p>- Led and motivated <Highlight>teams, delegating tasks, fostering collaboration, and ensuring effective communication</Highlight> among team members.</p>),
+                (<p>- Developed comprehensive <Highlight>project plans, defining scope, objectives, timelines, and resources</Highlight> required for successful project execution.</p>),
+            ]} />
+
+    },
+    {
+        title: "PlanetWalks LLC",
+        value: 3,
+        content: <Experience
+            heading={"Software Developer Intern"}
+            duration={"January, 2021 - July, 2021"}
+            accomplishements={[
+                (<p>- Learned and Developed <Highlight>backend APIs and application security</Highlight> focussing on <Highlight>quality, and optimized queries</Highlight> for faster data retrieving and transforming.</p>),
+                (<p>- Collaborated with frontend developers to <Highlight>design APIs</Highlight> facilitating seamless communication.</p>),
+                (<p>- Developed <Highlight>Streamlined Ordering System</Highlight> for the platform, <Highlight>optimizing database architecture and implementing secure authentication</Highlight> for enhanced user experience and scalability.</p>),
+                (<p>- Worked in an <Highlight>Early start-up environment</Highlight>, adeptly managing tight deadlines and high-pressure situations.</p>),
+                (<p>- Successfully documented backend system, <Highlight>facilitating seamless onboarding, enhancing code readability, and ensuring maintenance and troubleshooting</Highlight> of the platform.</p>),
+            ]}/>
+
+    },
+];
